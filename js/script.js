310a1b7a345729e9e4e7708624412c3e7d45c22b
DyegoIthalo <dyegoithalo11@hotmail.com>
2023-04-01 22:01:33 -0300

    perguntas frequentes animação

--- js/script.js
+++ js/script.js
@@ -16,3 +16,23 @@ links.forEach(ativarLink);
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
